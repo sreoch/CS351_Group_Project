9c0a24b7f6f64f918fc030c0531a351f82ac6254
Scott Reoch <scottreoch10@hotmail.co.uk>
2025-11-16 20:19:36 +0000

    changed a few things to improve the codebase

---
+++ src/shared/Constants.java
@@ -0,0 +1,15 @@
+package shared;
+
+public class Constants {
+    public static final int DEFAULT_PORT = 9000;
+    public static final int DEFAULT_THREAD_POOL_SIZE = 10;
+
+    public static final double STARTING_BALANCE = 1000.0;
+
+    public static final double DEFAULT_INTEREST_RATE = 0.025;
+    public static final int DEFAULT_INTEREST_PERIOD_SECONDS = 60;
+
+    private Constants() {
+
+    }
+}
